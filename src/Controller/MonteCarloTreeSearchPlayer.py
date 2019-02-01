09fd7f57bd5a1bfbb9199ae71c27a0cf176ea4f1
nugusha <nchkhaidze@wpi.edu>
2019-01-31 21:47:08 -0500

    New player implementing Monte Carlo Tree Search

---
+++ src/Controller/MonteCarloTreeSearchPlayer.py
@@ -0,0 +1,140 @@
+import numpy as np
+import math
+from RandomBot import RandomBot
+from TicTacToe import TicTacToeStatic
+
+class Node:
+    def __init__(self,s,cnt,x=1, parent=None):
+        self.s = np.array(s)
+        self.parent = parent 
+        self.c= [] 
+        self.x=x 
+        self.N=0 
+        self.w=0 
+        self.cnt = cnt
+
+    def expand(self):
+        status = TicTacToeStatic.Status(self.s)
+        if(status != None):
+            return
+
+        moves = TicTacToeStatic.available_moves(self.s)
+        if(self.cnt<=1000):
+            moves = TicTacToeStatic.removecopies(self.s,moves)
+
+        # print('--------------------')
+        # ss= self.s.copy()
+        # for move in moves:
+        #     ss[move[0]][move[1]]=9
+        # print(ss)
+        #print(self.x, " [[[[[")
+        #input()
+        turn = self.x
+        for move in moves:
+            new_s = self.s.copy()
+            r = move[0]
+            c = move[1]
+            new_s[r,c] = turn
+            next = Node(new_s,self.cnt+1,turn*-1,self)
+            self.c.append(next)
+
+    def rollout(self):
+        rand_play = RandomBot()
+        cur_s = self.s
+        turn = self.x
+
+        while(TicTacToeStatic.Status(cur_s)==None):
+            move = rand_play.make_a_move(cur_s)
+            r = move // len(self.s)
+            c = move % len(self.s)
+            new_s = cur_s.copy()
+            new_s[r,c]=turn
+            cur_s = new_s
+            turn*=-1
+
+        e = TicTacToeStatic.Status(cur_s)
+
+        return e
+
+    def backprop(self,e):
+        self.w += e
+        self.N += 1
+        if(self.parent != None):
+            self.parent.backprop(e)
+
+    def selection(self):
+        if(len(self.c) == 0):
+            return self
+
+        maxRate = -1
+        ind = 0
+
+        for i in range(len(self.c)):
+            child = self.c[i]
+
+            rate = 0
+            if(child.N == 0):
+                rate = float("inf")
+            else:
+                rate = UCB.UCB(self.x*child.w,child.N,self.N)
+            if(rate > maxRate or i == 0):
+                maxRate = rate
+                ind = i
+                
+        n = self.c[ind]
+
+        n = n.selection()
+        return n
+
+    def build_tree(self, n_iter=100):
+        for i in range(n_iter):
+            n = self.selection()
+            n.expand()
+            new_n = n.selection()
+            e = new_n.rollout()
+            new_n.backprop(e)
+
+class UCB:
+    @staticmethod
+    def UCB(wi,ni,N,c=1.142):
+        if(ni==0):
+            b = float("inf")
+        else:
+            b = wi/ni + c*math.sqrt(math.log(N)/ni)
+        return b
+
+class MonteCarloTreeSearchPlayer:
+    def __init__(self,x,name='MCTS'):
+        self.name = name
+        if(x==2):
+            x=-1
+        self.x = x
+
+    def make_a_move(self,s,pygame=0,n_iter=100):
+        s = np.array(s)
+        length = len(s)
+        cnt = 0
+        for i in range(length):
+            for j in range(length):
+                if(s[i][j]!=0):
+                    cnt += 1
+
+        if(cnt == 0):
+            return (len(s)//2-1)*len(s)+(len(s)//2-1)
+        
+        root = Node(s,cnt,self.x)
+        root.build_tree(n_iter)
+
+        most = 0
+        for child in root.c:
+            if(child.N>most):
+                most = child.N
+                C = child
+        
+        r,c = 0,0
+        length = len(s)
+        for i in range(length):
+            for j in range(length):
+                if(root.s[i,j]!=C.s[i,j]):
+                    r,c = i,j
+        return r*len(s)+c
---
+++ src/TicTacToe(10x10)MonteCarloTreeSearch_GUI.py
@@ -0,0 +1,17 @@
+from os import system
+import numpy as np
+import pygame
+import random
+import sys
+sys.path.append('./Controller')
+from TicTacToe import TicTacToe
+from RandomBot import RandomBot
+from HumanPlayer import HumanPlayer
+from MiniMaxPlayer import MiniMaxPlayer
+from MonteCarloTreeSearchPlayer import MonteCarloTreeSearchPlayer
+
+if __name__ == '__main__':
+    p1 = MonteCarloTreeSearchPlayer(1,"MCTS1")
+    p2 = MonteCarloTreeSearchPlayer(2,"MCTS2")
+    A_TicTacToe = TicTacToe(5,10,p1,p2)
+    A_TicTacToe.run()
