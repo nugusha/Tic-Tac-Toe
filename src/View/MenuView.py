055478d4e849d5d53f76edff37baf6694beccd73
nugusha <nchkhaidze@wpi.edu>
2019-02-08 18:02:34 -0500

    Fixed spacing in MenuView

--- src/View/MenuView.py
+++ src/View/MenuView.py
@@ -1,16 +1,19 @@
 import pygame
 import sys
 from TicTacToe import TicTacToe
+from Button import Button
+from Button import Text
+sys.path.append('./View')
 
-BLUE = (0,0,255)
-BLACK = (0,0,0)
-RED = (255,0,0)
-YELLOW = (255,255,0)
-WHITE = (255,255,255)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
 MENU_COLOR = (234, 255, 34)
 
 class MenuView:
-    def __init__(self,GRID_SIZE,players):
+    def __init__(self, GRID_SIZE, players):
         pygame.init()
         self.GRIDSIZE = GRID_SIZE
         self.SQUARESIZE = 300//GRID_SIZE
@@ -19,11 +22,11 @@ class MenuView:
         self.height = (GRID_SIZE + 2) * self.SQUARESIZE
         self.size = (self.width, self.height)
         self.screen = pygame.display.set_mode(self.size)
-        global Screen 
+        global Screen
         Screen = self.screen
 
     
-    def draw_Menu(self,players):
+    def draw_Menu(self, players):
         pygame.draw.rect(self.screen, MENU_COLOR, (0, 0, self.width, self.height ))
         pygame.display.update()
 
@@ -46,71 +49,25 @@ class MenuView:
         names_left = 27
         names_up = 8
         
-        Text.headline("Player 1",names_left,names_up,20)
-        Text.headline("Player 2",names_left+w,names_up,20)
+        Text.headline("Player 1", names_left, names_up, 20, self.screen)
+        Text.headline("Player 2", names_left+w, names_up, 20, self.screen)
         
         for i in range(number_of_players):
             x = LRMargin
             y = TOP+TDMargin+i*h
             
-            B1 = Button(self.screen,x,y,w,h,wm,hm,players[i].name)
-            B2 = Button(self.screen,x+w,y,w,h,wm,hm,players[i].name)
+            B1 = Button(self.screen, x, y, w, h, wm, hm, players[i].name)
+            B2 = Button(self.screen, x+w, y, w, h, wm, hm, players[i].name)
 
             buttons1.append(B1)
             buttons2.append(B2)
 
-        GRID3 = Button(self.screen,x+w/2-wm*1/3,TOP+TDMargin+(number_of_players)*h,w,h,wm*2/3,hm*4/5,"3x3")
-        GRID10 = Button(self.screen,x+w/2+wm*2/3,TOP+TDMargin+(number_of_players)*h,w,h,wm*2/3,hm*4/5,"10x10")
+        GRID3 = Button(self.screen, x+w/2-wm*1/3, TOP+TDMargin+(number_of_players)*h, w, h, wm*2/3, hm*4/5, "3x3")
+        GRID10 = Button(self.screen, x+w/2+wm*2/3, TOP+TDMargin+(number_of_players)*h, w, h, wm*2/3, hm*4/5, "10x10")
 
-        PlayButton = Button(self.screen,x+w/2,TDMargin+(number_of_players+1)*h,w/2,h/5*4,wm,hm*5/6,"PLAY")
+        PlayButton = Button(self.screen, x+w/2, TDMargin+(number_of_players+1)*h, w/2, h/5*4, wm, hm*5/6, "PLAY")
 
         pygame.display.update()
 
-        return buttons1,buttons2,PlayButton,GRID3,GRID10
-        
-class Text:
-    @staticmethod
-    def headline(headline,x,y,size):
-        def getFont(size):
-            return pygame.font.SysFont("monospace", size)
-        label = getFont(size).render(headline, 1 , BLACK)
-        Screen.blit(label, (x, y))
-        pygame.display.update()
+        return buttons1, buttons2, PlayButton, GRID3, GRID10
 
-class Button:
-    def __init__(self,screen,x,y,w,h,wm,hm,name):
-        self.screen = screen
-        self.color = [YELLOW,RED]
-        self.toggle = 0
-        self.name = name
-        self.x,self.y,self.w,self.h,self.wm,self.hm = x,y,w,h,wm,hm 
-        self.Toggle()
-
-    def Toggle(self):
-        self.draw_button(self.color[self.toggle])
-        self.toggle=1-self.toggle
-
-    def UnToggle(self):
-        self.draw_button(YELLOW)
-        self.toggle=1
-
-    def draw_button(self,color):
-        x,y,w,h,wm,hm = self.x,self.y,self.w,self.h,self.wm,self.hm
-        pygame.draw.rect(self.screen, BLACK, (x,y,wm,hm), 5)
-        k = 1
-        pygame.draw.rect(self.screen, color, (x+k,y+k,wm-k,hm-k))
-        
-        aName = self.name
-        Text.headline(aName,x+5,y+10,25)
-
-    def isClicked(self,event):
-        posx = event.pos[0]
-        posy = event.pos[1]
-        x,y,w,h = self.x,self.y,self.w,self.h
-        if(x<=posx and posx<=x+w):
-            if(y<=posy and posy<=y+h):
-                print(x,posx,x+w,y,posy,y+h)
-                print(x)
-                return 1
-        return 0
-    
